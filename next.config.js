820376e40fe28a0a7b812e3d0d73dad8cb85cfb3
Lakshya123-cyber <lakshyaraikwal123@gmail.com>
2023-06-15 15:56:59 +0800

    fii

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';
+`;
+const nextConfig = {
+  images: {
+    domains: ["fbsoabrjmkvucwrsxunv.supabase.co"],
+  },
+  key: "Content-Security-Policy",
+  value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
